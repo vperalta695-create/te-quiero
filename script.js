b5afa9010f7e9b9bfc6ccfa976205693762ca006
vperalta695-create <vperalta695@gmail.com>
2026-02-16 21:06:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// 🔐 CONTRASEÑA
-const password = "el 2 de febrero";
-
-const intento = prompt("💌 Esta carta tiene contraseña... ¿Cuál es?  pista el dia que fuimos novios" );
-
-if (intento !== password) {
-  document.body.innerHTML = `
-    <div style="display:flex;justify-content:center;align-items:center;height:100vh;font-family:sans-serif;background:#ffe6f0;">
-      <h2>❌ gracias no me queres </h2>
-    </div>
-  `;
-}
-
 // 💌 FUNCIÓN PARA ABRIR CARTA
 function abrirCarta() {
   document.querySelector(".carta").classList.toggle("abierta");
